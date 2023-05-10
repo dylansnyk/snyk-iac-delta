c03e103c9ebae7e8e0fc668d2ed2de8df565ef9d
Dylan Havelock <94395157+dylansnyk@users.noreply.github.com>
2023-05-10 16:50:40 -0400

    Create _old_main.tf

---
+++ exclude/_old_main.tf
@@ -0,0 +1,12 @@
+# Specify the provider and access details
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.14.0"
+    }
+  }
+}
+provider "aws" {
+  region = var.aws_region
+}
